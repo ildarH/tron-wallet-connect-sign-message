ba3c7ce2bbc43a01401141e59d53cf62c75b0ecb
ildarH <ildar.habibrahmanov@icloud.com>
2025-01-12 15:01:41 +0500

    Update script source path in index.html and modify Vite base configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/tron-wallet-connector/',
+  base: '/tron-wallet-connect-sign-message/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
